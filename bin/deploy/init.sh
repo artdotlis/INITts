ea9180b49e282297ca8407306eb122b9dc57ebb2
Artur Lissin <artur.lissin@dsmz.de>
2023-05-24 08:44:44 +0000

    tmp

--- bin/deploy/init.sh
+++ bin/deploy/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-root_path=$(dirname $(realpath $0))
+root_path=$(dirname "$(realpath "$0")")
 
 echo "install project"
 make build && make runBuild
 mkdir -p /var/www/
-mv $root_path/../../public/* /var/www/
+mv "$root_path"/../../public/* /var/www/
 echo "project installed"
