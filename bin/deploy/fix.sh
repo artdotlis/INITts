747a19dd6b682571af9e3669438bdaec3c00ba35
Artur Lissin <artur.lissin@dsmz.de>
2023-06-28 09:41:10 +0000

    build: improve deploy procedure

---
+++ bin/deploy/fix.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+root_path="$(dirname "$(realpath "$0")")/../.."
+
+echo "prepare for packaging"
+
+rm -rf "$root_path/extra"
+
+echo "create empty config"
+mkdir -p "$root_path/configs/src/initts/"
+if [ ! -f "$root_path/configs/src/initts/config.json" ]; then
+    cat "$root_path/bin/templates/config.json" >"$root_path/configs/src/initts/config.json"
+fi
+
+echo "preparing logos"
+mkdir -p "$root_path/extra/initts/logos"
+if [ ! "$(ls -A "$root_path/extra/initts/logos")" ]; then
+    touch "$root_path/extra/initts/logos/placeholder"
+fi
+
+echo "preparation finished"
